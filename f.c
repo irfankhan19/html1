53e33484dcfedc1ea9473d6485ca2e95a63c8c2f
Irfan Khan <Irfan122Khan@gmail.com>
2014-01-08 18:21:18 +0530

    some file added

---
+++ f.c
@@ -0,0 +1,8 @@
+Firefox is having trouble recovering your windows and tabs. This is usually caused by a recently opened web page.
+
+You can try:
+
+    Removing one or more tabs that you think may be causing the problem
+    Starting an entirely new browsing session
+
+
