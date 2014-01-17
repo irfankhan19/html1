1e7d056f418aca5eea4d66dd15535b903ee3dba5
Irfan Khan <Irfan122Khan@gmail.com>
2014-01-17 20:48:56 +0530

    add basic cart ability.

--- 2.c
+++ 2.c
@@ -1,8 +1,3 @@
-
-
-
-
-
 public class add{
 		public ststic void main(String args[]){
 		int a=20,b=30,c;
@@ -15,13 +10,16 @@ public class add{
 		c=(a+b);
 		System.out.println(c);
 	}
-}void main()
-{
-int i,n;
-clrscr();
-printf("enter the number");
-scanf("%d",&n);
-for(i=0;i<n;i++){
-Printf("%d",i);
-}
+}public class add{
+		public ststic void main(String args[]){
+		int a=20,b=30,c;
+		c=(a+b);
+		System.out.println(c);
+	}
+}public class add{
+		public ststic void main(String args[]){
+		int a=20,b=30,c;
+		c=(a+b);
+		System.out.println(c);
+	}
 }
